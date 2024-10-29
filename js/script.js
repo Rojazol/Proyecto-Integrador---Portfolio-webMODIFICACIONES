a509fada791a281808bfb9bac11deba2e98199b2
Rocio Jazmin Olabiaga <118866660+Rojazol@users.noreply.github.com>
2024-10-28 21:25:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,6 +16,50 @@ function seleccionar(){
     menuVisible = false;
 }
 
+//Funcion que aplica las animaciones de las habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        const imagenes = [
+            'ruta/a/imagen-html.png',
+            'ruta/a/imagen-css.png',
+            'ruta/a/imagen-csharp.png',
+            'ruta/a/imagen-sql.png',
+            'ruta/a/imagen-marketing.png',
+            'ruta/a/imagen-contabilidad.png',
+            'ruta/a/imagen-ensenanza.png',
+            'ruta/a/imagen-programacion.png'
+        ];
+        
+        for (let i = 0; i < habilidades.length; i++) {
+            habilidades[i].innerHTML = `<img src="${imagenes[i]}" alt="Habilidad ${i + 1}">`;
+        }
+    }
+}
+
+
+//detecto el scrolling para aplicar la animacion de la barra de habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+} 
+
+// cursos
+document.addEventListener('DOMContentLoaded', function () {
+    const cursos = document.querySelectorAll('.curso');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Deja de observar una vez que es visible
+            }
+        });
+    }, { threshold: 0.1 }); // El 10% del elemento debe estar en pantalla para activarse
+
+    cursos.forEach(curso => observer.observe(curso));
+});
 
 // Variable para rastrear el idioma actual (español por defecto)
 let isSpanish = true;
@@ -65,52 +109,3 @@ function toggleLanguage() {
     document.getElementById("language-toggle").textContent = isSpanish ? "Español" : "Inglés";
 }
 
-
-
-
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        const imagenes = [
-            'ruta/a/imagen-html.png',
-            'ruta/a/imagen-css.png',
-            'ruta/a/imagen-csharp.png',
-            'ruta/a/imagen-sql.png',
-            
-          
-'ruta/a/imagen-marketing.png',
-            'ruta/a/imagen-contabilidad.png',
-            'ruta/a/imagen-ensenanza.png',
-            'ruta/a/imagen-programacion.png'
-        ];
-        
-        for (let i = 0; i < habilidades.length; i++) {
-            habilidades[i].innerHTML = `<img src="${imagenes[i]}" alt="Habilidad ${i + 1}">`;
-        }
-    }
-}
-
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-} 
-
-// cursos
-document.addEventListener('DOMContentLoaded', function () {
-    const cursos = document.querySelectorAll('.curso');
-
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-                observer.unobserve(entry.target); // Deja de observar una vez que es visible
-            }
-        });
-    }, { threshold: 0.1 }); // El 10% del elemento debe estar en pantalla para activarse
-
-    cursos.forEach(curso => observer.observe(curso));
-});
